00349c8101c5c07d181f432f70f137014a897abd
Dongji Yang <dongji.yang@outlook.com>
2023-11-19 00:32:33 -0600

    login and register routes

---
+++ src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import RegistrationForm from "./_components/RegistrationForm";
+
+const RegisterPage: NextPage = () => {
+  return (
+    <main>
+      <RegistrationForm />
+    </main>
+  );
+};
+
+export default RegisterPage;
